56b2d4ad5e551a65494ee792601c95848c2fbe69
jasoncroci <jason_croci@yahoo.com>
2017-04-02 22:01:46 -0400

    Auto order when changing initiative

--- app/assets/javascripts/combats.js
+++ app/assets/javascripts/combats.js
@@ -18,10 +18,35 @@ $(document).ready(function() {
       }
     });
 
-    $('#combatant_table a[data-type="text"]').on('save', function(e, params) {
-      var field_id = $(this).attr("name") + "_field";
+    function updateField(item, params){
+      var field_id = item.attr("name") + "_field";
       $("#" + field_id).val(params.newValue)
+    }
+
+    function updateInitiativeOrder(item, params){
+      trs = $("tbody tr");
+      trs.each(function(i,tr){
+        var current_item = $(this).find("a[name$='_initiative']");
+
+        if( parseInt(params.newValue) > parseInt(current_item.html()) ){
+          current_item.closest("tr").before(item.closest("tr"));
+          return false;
+        }
+
+        // Lowest init
+        if( i === trs.length-1){
+          current_item.closest("tr").after(item.closest("tr"));
+        }
+      });
+    }
+
+    $('#combatant_table a[name$="_initiative"]').on('save', function(e, params) {
+      updateField( $(this), params );
+      updateInitiativeOrder( $(this), params );
     });
 
+    $('#combatant_table a[name$="_current_hit_points"]').on('save', function(e, params) {
+      updateField( $(this), params );
+    });
 
 });
