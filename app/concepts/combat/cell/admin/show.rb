3bd846ea8f74f997c5332aa033e788e73a8010d5
jasoncroci <jason_croci@yahoo.com>
2017-04-09 15:08:16 -0400

    Removing obsolete code

--- app/concepts/combat/cell/admin/show.rb
+++ app/concepts/combat/cell/admin/show.rb
@@ -1,15 +1,6 @@
 module Combat::Cell::Admin
   class Show < Combat::Cell::User::Show
     include Cell::Erb
-    include ActionView::Helpers::JavaScriptHelper
     property :encounter
-
-    def show
-      render + render("admin/javascript")
-    end
-
-    def admin?
-      true
-    end
   end
 end
