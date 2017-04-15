61b3e16296777fd191eed059d5508a7f9efb6008
jasoncroci <jason_croci@yahoo.com>
2017-04-15 15:51:15 -0400

    Better styling for encounters index page

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,7 +1,7 @@
 class Encounter < ApplicationRecord
   belongs_to :user
-  has_one :combat
-  has_many :enemies, -> { order(created_at: "asc") }
+  has_one :combat, dependent: :destroy
+  has_many :enemies, -> { order(created_at: "asc") }, dependent: :destroy
   scope :by_user, ->(user) { all.where(user:user) }
   accepts_nested_attributes_for :enemies, allow_destroy: true
 end
